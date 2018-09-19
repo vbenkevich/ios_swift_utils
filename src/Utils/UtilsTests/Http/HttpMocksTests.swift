ba385a99747e8ca665f4d56bf8b1a181ded1e56e
Vladimir Benkevich <vladimir.benkevich@devpool.com>
2018-09-19 12:16:58 +0300

    UrlSessionMockDecorator

---
+++ src/Utils/UtilsTests/Http/HttpMocksTests.swift
@@ -0,0 +1,32 @@
+//
+//  Created on 19/09/2018
+//  Copyright © Vladimir Benkevich 2018
+//
+
+import XCTest
+
+class HttpMocksTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
