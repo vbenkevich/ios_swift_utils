b7705d90ba3beb7e3f220c0068441d4b5de3a078
Vladimir Benkevich <vladimir.benkevich@gmail.com>
2018-10-30 20:06:10 +0300

    tests

--- src/JetLib/JetLibTests/Command/UIButtonCommandTests.swift
+++ src/JetLib/JetLibTests/Command/UIButtonCommandTests.swift
@@ -4,21 +4,109 @@
 //
 
 import XCTest
+@testable import JetLib
 
 class UIButtonCommandTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testCommandExecution() {
+        let expectation = self.expectation(description: "execute")
+        let button = UIButton()
+        button.command = ActionCommand(execute: { expectation.fulfill() })
+        button.sendActions(for: .touchUpInside)
+
+        wait(expectation)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+
+    func testCommandRemoving() {
+        let expectation = self.expectation(description: "execute")
+        let button = UIButton()
+
+        expectation.expectedFulfillmentCount = 1
+
+        button.command = ActionCommand(execute: { expectation.fulfill() })
+        button.sendActions(for: .touchUpInside)
+
+        button.command = nil
+        XCTAssertNil(button.command)
+
+        button.sendActions(for: .touchUpInside)
+
+        wait(expectation)
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+    func testCommandCanExecute() {
+        let button = UIButton()
+        button.command = ActionCommand(execute: { XCTFail() }, canExecute: { false })
+        XCTAssertFalse(button.isEnabled)
+
+        button.sendActions(for: .touchUpInside)
+    }
+
+    func testCommandParameterReftype() {
+        let expectation = self.expectation(description: "execute")
+        let button = UIButton()
+        let param = CommandParameterRef()
+
+        expectation.expectedFulfillmentCount = 1
+
+        button.command = ActionCommand(execute: { (p: CommandParameterRef) in
+            XCTAssertEqual(p, param)
+            expectation.fulfill()
+        }, canExecute: {
+            XCTAssertEqual($0, param)
+            return true
+        })
+
+        XCTAssertFalse(button.isEnabled)
+
+        button.commanParameter = param
+        XCTAssertTrue(button.isEnabled)
+
+        button.sendActions(for: .touchUpInside)
+        wait(expectation)
+
+        button.commanParameter = CommandParameterStruct()
+        XCTAssertFalse(button.isEnabled)
+    }
+
+    func testCommandParameterStruct() {
+        let expectation = self.expectation(description: "execute")
+        let button = UIButton()
+        let param = CommandParameterStruct()
+
+        expectation.expectedFulfillmentCount = 1
+
+        button.command = ActionCommand(execute: { (p: CommandParameterStruct) in
+            XCTAssertEqual(p, param)
+            expectation.fulfill()
+        }, canExecute: {
+            XCTAssertEqual($0, param)
+            return true
+        })
+
+        XCTAssertFalse(button.isEnabled)
+
+        button.commanParameter = param
+        XCTAssertTrue(button.isEnabled)
+
+        button.sendActions(for: .touchUpInside)
+        wait(expectation)
+
+        button.commanParameter = CommandParameterRef()
+        XCTAssertFalse(button.isEnabled)
+    }
+
+    class CommandParameterRef: Equatable {
+        static func == (lhs: CommandParameterRef, rhs: CommandParameterRef) -> Bool {
+            return lhs === rhs
+        }
+    }
+
+    struct CommandParameterStruct: Equatable {
+        let uui = UUID().uuidString
+
+        static func == (lhs: CommandParameterStruct, rhs: CommandParameterStruct) -> Bool {
+            return lhs.uui == rhs.uui
+        }
     }
 }
