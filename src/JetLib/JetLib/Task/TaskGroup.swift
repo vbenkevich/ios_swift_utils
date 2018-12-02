3c09a2bf972d028ef4e760cdad27bd269c12d30f
Vladimir Benkevich <vladimir.benkevich@gmail.com>
2018-12-02 17:03:31 +0300

    TaskGroup: cancel feature

--- src/JetLib/JetLib/Task/TaskGroup.swift
+++ src/JetLib/JetLib/Task/TaskGroup.swift
@@ -10,7 +10,7 @@ public protocol TaskGroupDelegate: class {
     func taskFinished(group: TaskGroup, task: NotifyCompletion)
 }
 
-public extension Array where Element: NotifyCompletion {
+public extension Array where Element: NotifyCompletion & Cancellable {
 
     func whenAll() -> Task<TaskGroup> {
         return TaskGroup(self).whenAll()
@@ -48,11 +48,11 @@ public extension TaskGroup {
     }
 }
 
-public final class TaskGroup {
+public final class TaskGroup: Cancellable {
 
     private let workQueue = DispatchQueue(label: "taskGroup")
 
-    public init(_ tasks: [NotifyCompletion]) {
+    public init(_ tasks: [NotifyCompletion & Cancellable]) {
         self.tasks = tasks
 
         if tasks.isEmpty {
@@ -66,7 +66,7 @@ public final class TaskGroup {
         subscribe(tasks)
     }
 
-    public let tasks: [NotifyCompletion]
+    public let tasks: [NotifyCompletion & Cancellable]
 
     public weak var delegate: TaskGroupDelegate?
 
@@ -100,6 +100,12 @@ public final class TaskGroup {
         }
 
         return tcs.task
+    }
+
+    public func cancel() {
+        for task in tasks {
+            try? task.cancel()
+        }
     }
 
     private func onCompleted(_ task: NotifyCompletion) {
