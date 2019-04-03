205460769e0518e73ffe727bdcda87185ae40a0f
Vladimir Benkevich <vladimir.benkevich@gmail.com>
2019-04-03 12:47:38 +0300

    equalSizeConststraint improvement

--- src/JetUI/JetUI/Utils/UIView.swift
+++ src/JetUI/JetUI/Utils/UIView.swift
@@ -12,7 +12,7 @@ public extension UIView {
         let container = UIView()
         container.translatesAutoresizingMaskIntoConstraints = false
         container.backgroundColor = UIColor.clear
-        container.insertSubview(self, at: 0)
+        container.addSubview(self)
 
         equalSizeConstraints(to: container)
 
@@ -20,12 +20,12 @@ public extension UIView {
     }
 
     @discardableResult
-    func equalSizeConstraints(to view: UIView, activate: Bool = true) -> [NSLayoutConstraint] {
+    func equalSizeConstraints(to view: UIView, insets: UIEdgeInsets = UIEdgeInsets.zero, activate: Bool = true) -> [NSLayoutConstraint] {
         let constraints = [
-            self.leftAnchor.constraint(equalTo: view.leftAnchor),
-            self.topAnchor.constraint(equalTo: view.topAnchor),
-            self.rightAnchor.constraint(equalTo: view.rightAnchor),
-            self.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+            self.leftAnchor.constraint(equalTo: view.leftAnchor, constant: insets.left),
+            self.topAnchor.constraint(equalTo: view.topAnchor, constant: insets.top),
+            self.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -insets.right),
+            self.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -insets.bottom)
         ]
 
         constraints.forEach { $0.isActive = activate }
